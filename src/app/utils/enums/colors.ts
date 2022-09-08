247d5a608c63633dc9f09c802e03aebb00f7cc65
lech <andrzejdlech@gmail.com>
2022-09-08 17:10:52 +0000

    deploy

--- src/app/utils/enums/colors.ts
+++
@@ -1,7 +0,0 @@
-export enum Colors{
-  WHITE = 'white',
-  BLUE = 'blue',
-  WRONG = 'wrong',
-  CORRECT = 'correct',
-  BLANK = ''
-}
--- src/app/utils/enums/settings.ts
+++
@@ -1,4 +0,0 @@
-export enum Settings{
-  NUMBER_OF_WORDS = 105,
-  TIME_LIMIT = 120,
-}
